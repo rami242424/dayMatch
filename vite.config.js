016b5a004c737374f6421be728f5a0a023e941f7
rami242424 <mickygamja@naver.com>
2026-08-20 20:22:49 +0900

    인앱 브라우저 호환성 개선 - CSS 중첩/논리 속성 제거, cssTarget 설정

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // 카카오톡 등 인앱 브라우저의 낮은 엔진 버전도 지원하기 위해 CSS 하위 호환 대상을 넓게 잡음
+    cssTarget: ['chrome80', 'safari13', 'ios13'],
+  },
 })
